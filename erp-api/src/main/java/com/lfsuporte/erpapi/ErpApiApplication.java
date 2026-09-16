6cf333e633ff943b097d825c8c414923f64dfeaa
Leonidas <leonidas@lfsuporte.com>
2026-09-16 13:37:09 -0300

    segurança: remove credenciais expostas do mongodb

--- erp-api/src/main/java/com/lfsuporte/erpapi/ErpApiApplication.java
+++ erp-api/src/main/java/com/lfsuporte/erpapi/ErpApiApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ErpApiApplication {
 
 	public static void main(String[] args) {
-		// A marreta: Forçando a conexão direto na memória antes do Spring subir!
-		System.setProperty("spring.data.mongodb.uri",
-				"mongodb+srv://admin:thr45h4tt4ck@cluster0.tkbfvyg.mongodb.net/lfsuporte?retryWrites=true&w=majority");
-
 		SpringApplication.run(ErpApiApplication.class, args);
 	}
 }
